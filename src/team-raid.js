1f9b54c39f50a968408a731b2551cfee1ae4fa70
Demitrious Kelly <demitrious@automattic.com>
2017-10-05 11:05:12 -0700

    click member from event to go to member page

--- src/team-raid.js
+++ src/team-raid.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
 class Raid extends Component {
+	memberClick = ( e ) => {
+		this.props.state.hasher.replace({main: "members", member: e.target.id})
+	}
 	members = () => {
 		var rval = []
 		for ( var i in this.props.data.members ) {
-			rval.push(<li key={"m-"+i} className="list-group-item">{this.props.data.members[i]}</li>)
+			rval.push(<li
+				key={"m-"+i}
+				id={this.props.data.members[i]}
+				onClick={this.memberClick}
+				style={{cursor: 'pointer'}}
+				className="list-group-item">{this.props.data.members[i]}</li>)
 		}
 		return rval
 	}
