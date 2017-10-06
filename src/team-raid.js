eda10287e394a33c81174b11610bed8c2547d691
Demitrious Kelly <demitrious@automattic.com>
2017-10-06 06:31:37 -0700

    no leave button on the member in the list...

--- src/team-raid.js
+++ src/team-raid.js
@@ -1,52 +1,64 @@
 import React, { Component } from 'react';
 
+class RaidMember extends Component {
+	click = () => {
+		this.props.state.hasher.replace({main: "members", member: this.props.id})
+	}
+	render = () => {
+		return (
+			<li
+				key={"m-"+this.props.ii}
+				id={this.props.id}
+				onClick={this.click}
+				style={{cursor: 'pointer'}}
+				className="list-group-item list-group-item-primary">{this.props.text}</li>
+		)
+	}
+}
+
 class Raid extends Component {
 	memberClick = ( e ) => {
+		console.log(e.target)
+		return
 		this.props.state.hasher.replace({main: "members", member: e.target.id})
 	}
 	members = () => {
 		var rval = []
 		for ( var i in this.props.data.members ) {
 			var text = (<div className="py-2">{this.props.data.members[i]}</div>)
-			var leave = null
-			if ( this.props.data.members[i] === this.props.state.user.name ) {
-				leave = (
-					<button className="btn btn-warning float-right" type="button">leave</button>
-				)
-			}
 			if ( i === "0" ) {
 				text = (<div className="py-2"><strong>{this.props.data.members[i]}</strong></div>)
 			}
-			rval.push(<li
-				key={"m-"+i}
-				id={this.props.data.members[i]}
-				onClick={this.memberClick}
-				style={{cursor: 'pointer'}}
-				className="list-group-item list-group-item-primary">{leave}{text}</li>)
+			rval.push(
+				<RaidMember key={"m-"+i}
+					id={this.props.data.members[i]}
+					state={this.props.state}
+					text={text}/>
+			)
+	}
+	return rval
+}
+alts = () => {
+	var rval = []
+	if ( this.props.data.alts !== null ) {
+		for ( var i in this.props.data.alts ) {
+			rval.push(
+				<li key={"a-"+i} className="list-group-item disabled">
+					{this.props.data.alts[i]}
+				</li>
+			)
 		}
-		return rval
 	}
-	alts = () => {
-		var rval = []
-		if ( this.props.data.alts !== null ) {
-			for ( var i in this.props.data.alts ) {
-				rval.push(
-					<li key={"a-"+i} className="list-group-item disabled">
-						{this.props.data.alts[i]}
-					</li>
-				)
-			}
-		}
-		return rval
-	}
-	render = () => {
-		return (
-			<ul className="list-group">
-				{this.members()}
-				{this.alts()}
-			</ul>
-		)
-	}
+	return rval
+}
+render = () => {
+	return (
+		<ul className="list-group">
+			{this.members()}
+			{this.alts()}
+		</ul>
+	)
+}
 }
 
 export default Raid
