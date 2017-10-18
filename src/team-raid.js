aabd2f9fb2302173e776bbf6ba4c6e6a0a3886fc
Demitrious Kelly <demitrious@automattic.com>
2017-10-17 23:48:17 -0700

    more explicitness

--- src/team-raid.js
+++ src/team-raid.js
@@ -25,7 +25,6 @@ class RaidMember extends Component {
 		var body = new URLSearchParams()
 		body.set('channel', this.props.state.vars.chan)
 		body.set('raid', raid.name)
-		console.log(body)
 		this.props.state.api.team.close(body)
 			.then(function() {
 				this.props.state.hasher.set({raid: null, chan: null})
@@ -41,7 +40,7 @@ class RaidMember extends Component {
 		if ( this.props.i === "0" ) {
 			colorClass = "btn-dark"
 		} else if ( this.props.data.need !== "0" && this.props.i >= this.props.data.need ) {
-			colorClass = "btn-link"
+			colorClass = "btn-light text-secondary"
 		}
 		var classes = "btn " + colorClass
 		if ( this.props.id === this.props.state.user.name ) {
@@ -91,6 +90,17 @@ class RaidMember extends Component {
 	}
 }
 
+class RaidPlaceHolder extends Component {
+	render = () => {
+		return (
+			<div className="my-1 w-100 btn-group" role="group">
+				<button type="button" className="w-75 btn btn-light text-secondary">member needed</button>
+				<button className={"btn btn-light w-25"} type="button">&nbsp;</button>
+			</div>
+		)
+	}
+}
+
 class Raid extends Component {
 	memberClick = ( e ) => {
 		this.props.state.hasher.replace({main: "members", member: e.target.id})
@@ -107,11 +117,36 @@ class Raid extends Component {
 		}
 		return rval
 	}
+	placeholders = () => {
+		if ( this.props.data.need < 1 ) {
+			return null
+		}
+		var list = this.combined()
+		var have = list.length
+		if ( have >= this.props.data.need ) {
+			return null
+		}
+		var rval = []
+		for (var i=0; i<=this.props.data.need-have; i++) {
+			rval.push((<RaidPlaceHolder key={i}/>))
+		}
+		return rval
+	}
 	members = () => {
 		var rval = []
 		var list = this.combined()
 		for ( var i in list ) {
+			if ( i === "0" ) {
+				rval.push((<div key="own" className="btn-group text-secondary px-3" role="group">⇊ owner</div>))
+			}
+			if ( i === "1" ) {
+				rval.push((<div key="mem" className="btn-group text-secondary px-3" role="group">⇊ members</div>))
+			}
+			if ( this.props.data.need > 0 && i == this.props.data.need ) {
+				rval.push((<div key="alt" className="btn-group text-secondary px-3" role="group">⇊ alternate queue</div>))
+			}
 			var member = list[i]
+
 			rval.push(
 				<RaidMember key={"m-"+i}
 					data={this.props.data}
@@ -127,6 +162,7 @@ class Raid extends Component {
 		return (
 			<div>
 				{this.members()}
+				{this.placeholders()}
 			</div>
 		)
 	}
