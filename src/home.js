d78fa4031bd83055766a9cb61655f0d130411e3a
Demitrious Kelly <demitrious@automattic.com>
2017-11-02 21:25:10 -0700

    more and less

--- src/home.js
+++ src/home.js
@@ -110,24 +110,29 @@ class Home extends Component {
 		var rval = []
 		for ( var i in this.state.games ) {
 			rval.push((
-				<div key={i} className="float-left w-50 my-1 mx-0 py-0 px-1">
-					<span style={{fontSize: "0.75em"}}>{this.state.games[i].name}</span>
-					<div className="progress">
-						<div className="progress-bar" style={{
-							width: this.state.games[i].pct + "%",
-							overflow: "elipses",
-						}}role="progressbar">
-						{this.state.games[i].players.toLocaleString()}
+				<div key={i} className="d-flex flex-row">
+					<div
+						className="text-nowrap w-75 mw-75 text-truncate"
+						style={{
+							paddingTop: "0.25em",
+							marginBottom: "0.1em",
+							height: "2em",
+							textOverflow: "ellipsis",
+							paddingLeft: "1em",
+							borderRadius: "0.5em",
+							backgroundImage: "-webkit-linear-gradient(left, #157EFB, #157EFB " + ( this.state.games[i].pct - 10 ) + "%, transparent "+this.state.games[i].pct+"%, transparent 100%)",
+					}}>
+						{this.state.games[i].name}
 					</div>
+					<div className="w-25 text-center">{this.state.games[i].players.toLocaleString()}</div>
 				</div>
-			</div>
 			))
 		}
 		return (
-			<div className="clearfix">
-				<div className="text-center">
-					<h6>Most active games</h6>
-					<h6>Over the last {this.state.days} days</h6>
+			<div>
+				<div className="d-flex flex-row">
+					<div className="w-75"><strong>Most PLayed Games</strong></div>
+					<div className="w-25 text-center"><strong>Players</strong></div>
 				</div>
 				{rval}
 			</div>
@@ -140,34 +145,86 @@ class Home extends Component {
 		var rval = []
 		for ( var i in this.state.stats ) {
 			rval.push((
-				<div key={i} className="float-left w-50 my-1 mx-0 py-0 px-1">
-					<span style={{fontSize: "0.75em"}}>{this.state.stats[i].name}</span>
-					<div className="progress">
-						<div className="progress-bar" style={{
-							width: this.state.stats[i].pct + "%",
-							overflow: "elipses",
-						}}role="progressbar">
-						{this.state.stats[i].v.toLocaleString()}
-					</div>
+				<div key={i} className="d-flex flex-row">
+					<div
+							className="text-nowrap w-75 mw-75 text-truncate"
+							style={{
+							paddingTop: "0.25em",
+							marginBottom: "0.1em",
+							height: "2em",
+								maxHeight: "2em",
+								textOverflow: "ellipsis",
+								paddingLeft: "1em",
+								borderRadius: "0.5em",
+								backgroundImage: "-webkit-linear-gradient(left, #157EFB, #157EFB " + ( this.state.stats[i].pct - 10 ) + "%, transparent "+this.state.stats[i].pct+"%, transparent 100%)",
+						}}>
+							{this.state.stats[i].name}
+						</div>
+					<div className="w-25 text-center">{this.state.stats[i].v.toLocaleString()}</div>
 				</div>
-			</div>
 			))
 		}
 		return (
-			<div className="clearfix">
-				<div className="text-center">
-					<h6>Most active channels</h6>
-					<h6>Over the last {this.state.days} days</h6>
+			<div>
+				<div className="d-flex flex-row">
+					<div className="w-75"><strong>Most Active Channels</strong></div>
+					<div className="w-25 text-center"><strong>Msgs</strong></div>
 				</div>
 				{rval}
 			</div>
 		)
 	}
+	longer = () => {
+		var newDays = this.state.days
+		switch( newDays ) {
+			default:
+				break
+			case 7:
+				newDays = 14
+				break;
+			case 14:
+				newDays = 30
+				break;
+			case 30:
+				newDays = 60
+				break;
+			case 60:
+				newDays = 90
+				break;
+		}
+		this.setState({days: newDays, fetching: false})
+	}
+	shorter = () => {
+		var newDays = this.state.days
+		switch( newDays ) {
+			default:
+				break
+			case 14:
+				newDays = 7
+				break;
+			case 30:
+				newDays = 14
+				break;
+			case 60:
+				newDays = 30
+				break;
+			case 90:
+				newDays = 60
+				break;
+		}
+		this.setState({days: newDays, fetching: false})
+	}
 	render = () => {
 		return (
 			<div>
+				<form>
+				<div className="form-group clearfix">
+					<button style={{marginLeft: "12.5%" }} className="btn-sm btn-primary float-left w-25 btn" onClick={this.shorter}>fewer</button>
+					<input disabled className="mx-1 form-control-sm text-center float-left w-25 m-0 form-text form-control" value={this.state.days+" days"} type="text"/>
+					<button className="btn-sm btn-primary float-left w-25 btn" onClick={this.longer}>more</button>
+				</div>
+				</form>
 				{this.slack()}
-				<br/>
 				{this.games()}
 			</div>
 		)
