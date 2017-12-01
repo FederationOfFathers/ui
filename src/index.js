32a4d1bfe0e9582bebb35c656cbbc5a8415662e4
Demitrious Kelly <demitrious@automattic.com>
2017-11-30 19:37:59 -0800

    polyfill differently...

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'url-search-params-polyfill';
+
 import Promise from 'promise-polyfill';
 if (!window.Promise) {
 	window.Promise = Promise;
