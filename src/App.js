211d666a058adc103e46faac1ce84d06ec015549
Bear Cherian <cherian.benjamin@gmail.com>
2019-03-03 09:28:39 -0500

    Updating invite link. Adding text to logout for old member

--- src/App.js
+++ src/App.js
@@ -34,14 +34,15 @@ class App extends Api {
 	pleaseLogIn = () => {
 
 		const discordLoginURL = "https://discordapp.com/api/oauth2/authorize?client_id=447447191640997888&redirect_uri=https%3A%2F%2Fdashboard.fofgaming.com%2Fapi%2Fv1%2Foauth%2Fdiscord%2Flogin&response_type=code&scope=identify&state=login"
+		const discordInviteURL = "https://discord.gg/WAgSs2Y";
 		return(
 			<div className="app noauth text-left my-5">
 				<div className="text-center">
 					<h2>Login With Discord</h2>
 					<div className="field discord">
-						<p className="helper-text">You need to be verified in the <a href="https://discord.gg/8H2stfJ">FoF Discord</a> to be able to login.</p>
+						<p className="helper-text">You need to be verified in the <a href={discordInviteURL}>FoF Discord</a> to be able to login.</p>
 						<a className="btn btn-block btn-lg text-center" style={{ backgroundColor: "#7289DA"}} href={discordLoginURL}><img className="img-fluid h-100" src="/images/discord-logo-white.png" alt="Discord"/></a>
-						<p><a href="https://discord.gg/8H2stfJ">Join FoF! - https://discord.gg/8H2stfJ</a></p>
+						<p><a href={discordInviteURL}>Join FoF! - {discordInviteURL}</a></p>
 						<small className="helper-text text-black-50">Slack is no more. All hail Discord!</small><br/>
 						<small className="text-black-50">if this doesn't work, it's because it's not done</small>
 					</div>
@@ -55,7 +56,13 @@ class App extends Api {
 			return this.pleaseLogIn()
 		}
 		if ( false === this.state.verified  ) {
-			return (<div className="alert alert-danger">You must be verified to use the team tool<br/><Logout/></div>)
+			return (
+				<div className="alert alert-danger">
+					<p>You must be verified to use the team tool.</p>
+					<p>If you haven't logged in with Discord before, logout, and then log back in with Discord.</p>
+					<Logout/>
+				</div>
+			)
 		}
 		return ( <LoggedIn state={this.state}/> );
 	}
