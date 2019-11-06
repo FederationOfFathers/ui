372d7a5d93bd390bb4596a5800ebb5614d50133f
Bear Cherian <bearcherian@gmail.com>
2019-11-05 23:40:04 -0600

    Adding updated build files

---
+++ build/precache-manifest.46026e1b4782fb046199aa71b91bfd4b.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af5b2f01e108cbf536aebfd6f58ca98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eda491ab0504c107f9ec",
+    "url": "/static/css/main.c742f742.css"
+  },
+  {
+    "revision": "eda491ab0504c107f9ec",
+    "url": "/static/js/main.19b0b431.js"
+  }
+]);
